1135734986ccfca29bf414414956ed91bae0e720
Marco Ferra <maferra@icloud.com>
2019-12-19 15:27:08 +0000

    Committed.

--- deploy.cmd
+++ deploy.cmd
@@ -6,8 +6,5 @@ git add --all .
 git commit -S -m "Committed."
 git push
 
-rem remove all files
-call aws s3 rm s3://doctorflow.net --recursive
-
 rem start s3 sync
-call aws s3 sync public s3://doctorflow.net
+call aws s3 sync public s3://doctorflow.net --delete
